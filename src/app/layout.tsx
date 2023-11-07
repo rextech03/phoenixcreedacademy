e4dd18b4758485189e55d8d35a041d3c795e9b5f
mark ogujiuba <markogujiuba@gmail.com>
2023-11-07 18:45:27 +0100

    frontpage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 import NavBar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,7 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
       <NavBar />
         {children}
-     
+     <Footer />
         </body>
     </html>
   )
